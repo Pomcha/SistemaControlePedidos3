cbfa56157d8d6217e15e910fa9e4902098249c6e
anajufrancelin <franceliana007@gmail.com>
2025-12-04 07:41:04 -0300

    Implementacao da funcao analisarCliente e verificadores de ID/CPF

--- src/cliente.c
+++ src/cliente.c
@@ -1,5 +1,6 @@
 //struct_cliente:
 #include <stdio.h>
+#include <string.h>
 typedef struct{
     int id;
     char tipo[2];
@@ -14,3 +15,33 @@ typedef struct{
     Cliente dados;
     char cpf[20];
 } PessoaFisica;
+
+// funcao: verificar se ID ja existe
+int verificarID(int id, Cliente lista[], int total) {
+    for(int i = 0; i < total; i++) {
+        if(lista[i].id == id) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//funcao: verificar se CPF ja existe
+int verificarCPF(char cpf[], PessoaFisica lista[], int total) {
+    for(int i = 0; i < total; i++) {
+        if(strcmp(lista[i].cpf, cpf) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//analisar_cliente
+int analisarCliente(int id, char documento[], Cliente listaCliente[], int totalClientes, PessoaFisica listaPF[], int totalPF) {
+    if(verificarID(id, listaCliente, totalClientes))
+    return 1;
+    if(verificarCPF(documento, listaPF, totalPF))
+    return 2;
+
+    return 0;
+}
