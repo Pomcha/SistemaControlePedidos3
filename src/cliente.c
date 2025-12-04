a708c03399ea0e84f4be0d838b7c8692bc697ed3
anajufrancelin <franceliana007@gmail.com>
2025-12-04 08:05:25 -0300

    Implementacao da funcao cadastrarCliente

--- src/cliente.c
+++ src/cliente.c
@@ -45,3 +45,55 @@ int analisarCliente(int id, char documento[], Cliente listaCliente[], int totalC
 
     return 0;
 }
+
+//cadastrar_cliente
+int cadastrarCliente(Cliente listaClientes[], int *totalClientes, PessoaFisica listaPF[], int *totalPF) {
+    int id;
+    char nome[100], endereco[200], telefone[20], email[100];
+    char cpf[20];
+    printf("Digite o ID do cliente: ");
+    scanf("%d", &id);
+
+    printf("Digite o nome: ");
+    scanf(" %[^\n]", nome);
+
+    printf("Digite o endereco: ");
+    scanf(" %[^\n]", endereco);
+
+    printf("Digite o telefone: ");
+    scanf(" %[^/n]", telefone);
+
+    printf("Digite o email: ");
+    scanf(" %[^\n]", email );
+
+    printf("Digite o CPF: ");
+    scanf(" %[^\n]", cpf);
+
+    int status = analisarCliente(id, cpf, listaClientes, *totalClientes, listaPF, *totalPF);
+    if(status==1) {
+        printf("ERRO: ID ja cadastrado\n");
+        return 1;
+    }
+    if(status==2){
+        printf("ERRO: CPF ja cadastrado\n");
+    }
+    listaClientes[*totalClientes].id = id;
+    strcpy(listaClientes[*totalClientes].tipo, "F");
+    strcpy(listaClientes[*totalClientes].nome, nome);
+    strcpy(listaClientes[*totalClientes].endereco, endereco);
+    strcpy(listaClientes[*totalClientes].telefone, telefone);
+    strcpy(listaClientes[*totalClientes].email, email);
+    (*totalClientes)++;
+
+    listaPF[*totalPF].dados.id = id;
+    strcpy(listaPF[*totalPF].dados.nome, nome);
+    strcpy(listaPF[*totalPF].dados.endereco, endereco);
+    strcpy(listaPF[*totalPF].dados.telefone, telefone);
+    strcpy(listaPF[*totalPF].dados.email, email);
+    strcpy(listaPF[*totalPF].cpf, cpf);
+    (*totalPF)++;
+
+    printf("Cliente cadastrado com sucesso!\n");
+    return 0;
+
+}
